ff024f7507bb3f1ea77a899051e28c417a0d20a7
Afshar07 <mr.afshar0@gmail.com>
2021-05-16 23:28:03 +0430

    Set the images on place using grid system

--- src/components/UI/StepOne.vue
+++ src/components/UI/StepOne.vue
@@ -1,29 +1,54 @@
 <template>
-  <div :style="style">
-    <option-container
-      :optionImg="paperImg"
-      style="background-image: linear-gradient(#4865f4, #5671f5);"
-      :style="stepOneStyle"
-    ></option-container>
-    <option-container
-      :optionImg="scissorImg"
-      :style="stepOneStyle"
-      style="background-image: linear-gradient(#ec9e0e, #eca922);"
-    ></option-container>
-    <option-container
-      :optionImg="rockImg"
-      :style="stepOneStyle"
-      style="background-image: linear-gradient(#dc2e4e, #dd405d);"
-    ></option-container>
+  <div :style="style" class="container">
+    <div class="grid-container">
+      <option-container
+      class="paper"
+        :optionImg="paperImg"
+        style="background-image: linear-gradient(#4865f4, #5671f5);"
+        :style="stepOneStyle"
+      ></option-container>
+      <option-container
+      class="scissor"
+        :optionImg="scissorImg"
+        :style="stepOneStyle"
+        style="background-image: linear-gradient(#ec9e0e, #eca922);"
+      ></option-container>
+      <option-container
+      class="rock"
+        :optionImg="rockImg"
+        :style="stepOneStyle"
+        style="background-image: linear-gradient(#dc2e4e, #dd405d);"
+      ></option-container>
+    </div>
   </div>
 </template>
 
 <style scoped>
-div {
+.container {
   width: 50vw;
   height: 30rem;
   background-repeat: no-repeat;
   background-position: center center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.grid-container{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  align-items: center;
+  justify-items: center;
+}
+.paper{
+  grid-column: 2/3;
+}
+.scissor{
+  grid-column: 5/6;
+}
+.rock{
+  grid-column:3/5;
+  grid-row: 3/ -2;
 }
 </style>
 
