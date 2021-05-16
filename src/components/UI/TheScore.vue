fcba9de85370b723fbda921414471090ac5e3c0c
Afshar07 <mr.afshar0@gmail.com>
2021-05-16 17:13:37 +0430

    Add the header section

---
+++ src/components/UI/TheScore.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header-wrapper">
+    <img :src="logo" />
+    <div>Score 0</div>
+  </div>
+</template>
+
+<style scoped></style>
+<script>
+export default {
+  props: ["logo"],
+};
+</script>
