0ca0eea1083ffa9645d71204bf335b451284bbf5
Afshar07 <mr.afshar0@gmail.com>
2021-05-16 21:08:47 +0430

    Create OptionContainer component to show rock,paper and scissor option

---
+++ src/components/Layouts/OptionContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container" :style="style">
+    <div class="whiteBg">
+      <img :src="optionImg" alt="Choose!" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+    .container{
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+    }
+    .whiteBg{
+        background-color: #eee;
+        width: 80%;
+        height: 80%;
+        border-radius: 50%;
+                display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
+
+<script>
+export default {
+  props: ["optionImg"],
+};
+</script>
