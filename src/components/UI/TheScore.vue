ed679bbc5d6c7d75a911cfe97731d66bc509812b
Afshar07 <mr.afshar0@gmail.com>
2021-05-16 17:44:03 +0430

    change the width unit to vw

--- src/components/UI/TheScore.vue
+++ src/components/UI/TheScore.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="header-wrapper">
     <img :src="logo" />
-    <div>Score 0</div>
+    <div class="scoreContainer">
+      <p>score</p>
+      <p class="score">{{ score }}</p>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header-wrapper {
+  width: 50vw ;
+  display: flex;
+  justify-content: space-between;
+  border: 2px solid #606e85;
+  padding: 1rem;
+  margin-top: 1rem;
+  border-radius: 10px;
+}
+.scoreContainer {
+  width: 25%;
+  height: 80%;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+}
+.scoreContainer p {
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  color: #2a46c0;
+}
+.score{
+  font-size: 2.3rem;
+  margin: 0 0 1rem 0;
+  color: #3b4363 !important;
+}
+</style>
 <script>
 export default {
-  props: ["logo"],
+  props: ["logo", "score"],
 };
 </script>
