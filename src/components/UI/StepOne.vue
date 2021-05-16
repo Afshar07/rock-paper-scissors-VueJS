beeb70e0082f5a52f9b5709980ba4d2221a814dd
Afshar07 <mr.afshar0@gmail.com>
2021-05-16 21:08:10 +0430

    import and use OptionContainer component and styling

--- src/components/UI/StepOne.vue
+++ src/components/UI/StepOne.vue
@@ -1,6 +1,20 @@
 <template>
   <div :style="style">
-    
+    <option-container
+      :optionImg="paperImg"
+      style="background-image: linear-gradient(#4865f4, #5671f5);"
+      :style="stepOneStyle"
+    ></option-container>
+    <option-container
+      :optionImg="scissorImg"
+      :style="stepOneStyle"
+      style="background-image: linear-gradient(#ec9e0e, #eca922);"
+    ></option-container>
+    <option-container
+      :optionImg="rockImg"
+      :style="stepOneStyle"
+      style="background-image: linear-gradient(#dc2e4e, #dd405d);"
+    ></option-container>
   </div>
 </template>
 
@@ -14,12 +28,22 @@ div {
 </style>
 
 <script>
+import OptionContainer from "../Layouts/OptionContainer.vue";
 export default {
+  components: { OptionContainer },
   props: ["backUrl"],
   computed: {
     style() {
       return "background-image:" + `url(${this.backUrl})`;
     },
+  },
+  data() {
+    return {
+      rockImg: require("../../assets/icon-rock.svg"),
+      paperImg: require("../../assets/icon-paper.svg"),
+      scissorImg: require("../../assets/icon-scissors.svg"),
+      stepOneStyle: "width: 8rem; height: 8rem",
+    };
   },
 };
 </script>
