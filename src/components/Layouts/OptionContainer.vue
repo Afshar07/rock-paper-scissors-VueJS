5026a345fdc89cdadee27ec738b64165274477fe
Afshar07 <mr.afshar0@gmail.com>
2021-05-17 00:51:03 +0430

    Add box shadow on hover

--- src/components/Layouts/OptionContainer.vue
+++ src/components/Layouts/OptionContainer.vue
@@ -7,24 +7,26 @@
 </template>
 
 <style scoped>
-    .container{
-        border-radius: 50%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
-    }
-    .whiteBg{
-        background-color: #eee;
-        width: 80%;
-        height: 80%;
-        border-radius: 50%;
-                display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.container {
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.whiteBg {
+  background-color: #eee;
+  width: 80%;
+  height: 80%;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.whiteBg:hover {
+  box-shadow: 1px 1px 15px 1px #6e6e6e inset;
+}
 </style>
-
 
 <script>
 export default {
