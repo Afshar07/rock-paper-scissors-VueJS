44f61eb0975dee9082844293a74c6431e0c8159f
Afshar07 <mr.afshar0@gmail.com>
2021-05-16 18:01:58 +0430

    Add the background image and slots

---
+++ src/components/UI/TheWrapper.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :style="style">
+    <slot name="paper"></slot>
+    <slot name="scissor"></slot>
+    <slot name="rock"></slot>
+  </div>
+</template>
+
+<style scoped>
+div {
+  width: 50vw;
+  height: 30rem;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+</style>
+
+<script>
+export default {
+  props: ["backUrl"],
+  computed: {
+    style() {
+      return "background-image:" + `url(${this.backUrl})`;
+    },
+  },
+};
+</script>
