49bd789819531b672d9ce13e40a6aec4d3d8c99f
Afshar07 <mr.afshar0@gmail.com>
2021-05-17 16:15:11 +0430

    Add the prototype method for paper

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,40 @@
   <keep-alive>
     <the-score :logo="logoUrl" :score="score"></the-score>
   </keep-alive>
-  <step-one :backUrl="backgroundImg"></step-one>
+  <step-one
+    @paper="playerHasChoosenPaper()"
+    v-if="currentStep === 'step-one'"
+    :backUrl="backgroundImg"
+  ></step-one>
+  <step-two
+    v-else-if="currentStep === 'step-two'"
+    :choosen="stepTwoClass"
+    :choosenImg="choosenImg"
+  ></step-two>
 </template>
 
 <script>
 import TheScore from "./components/UI/TheScore.vue";
 import StepOne from "./components/UI/StepOne.vue";
+import StepTwo from "./components/UI/StepTwo.vue";
 export default {
-  components: { TheScore, StepOne },
+  components: { TheScore, StepOne, StepTwo },
   data() {
     return {
+      currentStep: "step-one",
+      stepTwoClass: "",
       logoUrl: require("./assets/logo.svg"),
       score: "0",
       backgroundImg: require("./assets/bg-triangle.svg"),
+      choosenImg: "",
     };
+  },
+  methods: {
+    playerHasChoosenPaper() {
+      this.currentStep = "step-two";
+      this.stepTwoClass = "paperStyle";
+      this.choosenImg = require("./assets/icon-paper.svg");
+    },
   },
 };
 </script>
@@ -23,7 +43,7 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap");
 body {
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   margin: 0 !important;
   padding: 0 !important;
