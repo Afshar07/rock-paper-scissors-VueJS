6ef217d8afcc18b222cc9b6176ffc8fbd0ab8443
Afshar07 <mr.afshar0@gmail.com>
2021-05-17 19:07:18 +0430

    add methods for get pc choice and determine the winner

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
     v-else-if="currentStep === 'step-two'"
     :choosen="stepTwoClass"
     :choosenImg="choosenImg"
+    @rendered="onRenderChild($event)"
   ></step-two>
 </template>
 
@@ -30,23 +31,60 @@ export default {
       score: "0",
       backgroundImg: require("./assets/bg-triangle.svg"),
       choosenImg: "",
+      pcChoosen: "",
+      playerChoosen: "",
+      winner: "",
     };
   },
+  watch: {
+    pcChoosen() {
+      this.determineWinner();
+    },
+    winner(value) {
+      if (value == "player") {
+        this.score++;
+      } else if (value == "draw") {
+        return;
+      } else {
+        this.score--;
+      }
+    },
+  },
+
   methods: {
+    determineWinner() {
+      const winningMap = { rock: "scissor", paper: "rock", scissor: "paper" };
+      if (this.pcChoosen == this.playerChoosen) {
+        this.winner = "draw";
+      } else if (this.pcChoosen === winningMap[this.playerChoosen]) {
+        this.winner = "player";
+      } else {
+        this.winner = "computer";
+      }
+      console.log(winningMap[this.playerChoosen]);
+      return;
+    },
     playerHasChoosenPaper() {
       this.currentStep = "step-two";
       this.stepTwoClass = "paperStyle";
       this.choosenImg = require("./assets/icon-paper.svg");
+      this.playerChoosen = "paper";
     },
     playerHasChoosenScissor() {
       this.currentStep = "step-two";
       this.stepTwoClass = "scissorStyle";
       this.choosenImg = require("./assets/icon-scissors.svg");
+      this.playerChoosen = "scissor";
     },
     playerHasChoosenRock() {
       this.currentStep = "step-two";
       this.stepTwoClass = "rockStyle";
       this.choosenImg = require("./assets/icon-rock.svg");
+      this.playerChoosen = "rock";
+    },
+    onRenderChild(value) {
+      this.pcChoosen = value;
+      console.log(this.pcChoosen);
     },
   },
 };
