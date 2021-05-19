7622ae632b4fe66b429f8b7e2ab2b2c2580ea21f
Afshar07 <mr.afshar0@gmail.com>
2021-05-19 17:34:45 +0430

    Add emit and style the btn

--- src/components/UI/TheRulesButton.vue
+++ src/components/UI/TheRulesButton.vue
@@ -1,23 +1,23 @@
 <template>
-    <button>rules</button>
+  <button @click="$emit('openRules')">rules</button>
 </template>
 
 <script>
 export default {
-    
-}
+    emits: ["openRules"]
+};
 </script>
 <style scoped>
-button{
-    width: 10vw;
-    height: 6vh;
-    border-radius: 6px;
-    font-weight: 700;
-    color: #fff;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    background: transparent;
-    border: 1px solid #fff;
-    cursor: pointer;
+button {
+  width: 10vw;
+  height: 6vh;
+  border-radius: 6px;
+  font-weight: 700;
+  color: #fff;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  background: transparent;
+  border: 1px solid #fff;
+  cursor: pointer;
 }
 </style>
