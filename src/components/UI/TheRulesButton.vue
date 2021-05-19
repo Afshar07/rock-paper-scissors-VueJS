9fc6ffb0eb19895114b60be641036be33ebaed81
Afshar07 <mr.afshar0@gmail.com>
2021-05-19 16:51:14 +0430

    Create and style the rules btn component

---
+++ src/components/UI/TheRulesButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button>rules</button>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+button{
+    width: 10vw;
+    height: 6vh;
+    border-radius: 6px;
+    font-weight: 700;
+    color: #fff;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    background: transparent;
+    border: 1px solid #fff;
+    cursor: pointer;
+}
+</style>
